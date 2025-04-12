24764aabdeea543035b50d448848763924a1a9d7
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-04-11 18:51:19 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
-try {
-    $connect = new PDO("mysql:host=localhost;dbname=test", "root", "");
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error al conectar con la base de datos: " . $e->getMessage());
+$host = "sql306.infinityfree.com"; 
+$user = "if0_38238326";
+$pass = "ajzFCRUmYgyIOa";
+$dbname = "if0_38238326_NewTest";
+
+$conexion = new mysqli($host, $user, $pass, $dbname);
+
+if ($conexion->connect_error) {
+    die("Connection failed: " . $conexion->connect_error);
+} else {
+    echo "Connected successfully!";
 }
 ?>
